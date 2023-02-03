3f4997a862f4ed11dd73f40897010ba096d80ff1
thddudco-dev <121590967+thddudco-dev@users.noreply.github.com>
2023-02-04 06:26:50 +0900

    Firebase 설정
    파이어 베이스 설정.

--- dapa/dapaApp.swift
+++ dapa/dapaApp.swift
@@ -6,11 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct dapaApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     let persistenceController = PersistenceController.shared
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
